847d000e3bc460c34541b3ceb2a4dbef1be05ab6
digitalcreation <kvanburen@fullsail.edu>
2014-09-19 23:45:42 -0700

    Added comments

--- Wk3/goal4_assign_duel3/js/main.js
+++ Wk3/goal4_assign_duel3/js/main.js
@@ -18,13 +18,18 @@ DEVELOP Duel #3
     //console.log("FIGHT!!!");
     //Logs the string "FIGHT!!!" in the console
 
+    //Setups variable for players 1&2 that will show name and health on the page.
     var player1_txt = document.querySelector("#kabal").querySelector("p");
     var player2_txt = document.querySelector("#kratos").querySelector("p");
+    //Setups variable for the current round on the page.
     var round_txt = document.querySelector("#round_number");
+    //Setups variable for the fight button on the page.
     var button = document.getElementById("fight_btn");
 
+    //sets up click event
     button.addEventListener("click", fight, false);
 
+    //Creates an array of objects for 2 players
     var players = [
         {
             name:"Kabal",
@@ -36,9 +41,11 @@ DEVELOP Duel #3
             damage:20,
             health:100
         }];
+
     //Creates a variable for the round number, which starts at 1.
     var round = 1;
 
+    //Initialize DOM innerHTML text for HTML page.
     round_txt.innerHTML = "Click FIGHT BUTTON to Start!";
     player1_txt.innerHTML = players[0].name + ":" + players[0].health;
     player2_txt.innerHTMl = players[1].name + ":" + players[1].health;
@@ -47,6 +54,7 @@ DEVELOP Duel #3
     function fight(){
 
         //alert(player1[0]+":"+player1[2]+"  *START*  "+player2[0]+":"+player2[2]);
+        //Prints the name and current health of each player
         player1_txt.innerHTML = players[0].name + ":" + players[0].health;
         player2_txt.innerHTML = players[1].name + ":" + players[1].health;
 
@@ -55,34 +63,37 @@ DEVELOP Duel #3
             var p1 = Math.floor(Math.random() * players[0].damage + players [0].damage * .5);
             var p2 = Math.floor(Math.random() * players[1].damage + players [1].damage * .5);
 
-            //inflict damage
+            //inflicts damage
             players[0].health -= p1;
             players[1].health -= p2;
 
             //Logs the result of the damage done to each player in the console.
             console.log(players[0].health, players[1].health);
-
+            //Setups variable that will run the winnerCheck function which will determine if there is a winner or not.
             var result = winnerCheck();
+            //Logs the results in the console.
             console.log(result);
-
+            //Prints the current round and results on the HTML page.
             round_txt.innerHTML = "ROUND #" + round + " Results:";
+            //Increments the current round by 1.
             round++;
 
-            //Logs winner in the console.
-            if (result === "no winner")
             //If the result is strictly equal to "no winner" then continue loop.
+            if (result === "no winner")
             {
+                //If there is no winner, print each players name and current health on the HTML page.
                 player1_txt.innerHTML = players[0].name + ":" + players[0].health;
                 player2_txt.innerHTML = players[1].name + ":" + players[1].health;
             } else{
+                //Otherwise, print the results in the player1 name/health area on the HTML page and empty player 2's area.
                 player1_txt.innerHTML = result;
                 player2_txt.innerHTML = "";
 
+                //Disable the button.
                 button.removeEventListener("click", fight, false);
 
                 document.querySelector(".buttonblue").innerHTML = "DONE!!!";
             };
-
 
     };
 
